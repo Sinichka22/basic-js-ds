1741518609a899e8c576d3b6f6b4ff60a9b54c7f
Aliaksandra <a.sinitsyna07@gmail.com>
2023-12-24 23:05:35 +0100

    feat: add task

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -12,23 +12,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+let stack = [];
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push(element) {
+    stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (stack.length === 0) {
+      return undefined;
+    }
+    return stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (stack.length === 0) {
+      return undefined;
+    }
+    return stack[stack.length - 1];
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
