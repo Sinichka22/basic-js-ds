690ec55156f8661556201e94b6b978704ce3b172
Aliaksandra <a.sinitsyna07@gmail.com>
2023-12-25 12:57:45 +0100

    feat: add task

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,24 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
 class Queue {
+  constructor() {
+    this.storage = [];
+    this.head = 0;
+    this.tail = 0;
+  }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    let result = [];
+    let obj = {};
+    let objNext = {};
+    for (let i = this.storage.length - 1; i >= 0; i--) {
+      if (i == this.storage.length - 1) {
+        objNext = { value: this.storage[i], next: null };
+        result.push(objNext);
+      } else {
+        obj = { value: this.storage[i], next: objNext };
+        objNext = obj;
+        result.push(obj);
+      }
+    }
+    return obj;
+
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    this.storage.push(value);
+    this.tail++;
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    let el = this.storage.shift();
+    return el;
     // remove line with error and write your code here
   }
 }
+/*const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(5);
+queue.dequeue();
+queue.getUnderlyingList();*/
 
 module.exports = {
-  Queue
+  Queue,
 };
